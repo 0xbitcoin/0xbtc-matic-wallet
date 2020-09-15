834aa4e1bd10029aaffc18854caaaa117aa3b5c2
andy <andy@jonezie.com>
2020-09-15 10:59:47 -0400

    adding purpose statement

--- src/views/Docs.vue
+++ src/views/Docs.vue
@@ -26,14 +26,31 @@
           <div class="m-6 p-4 bg-gray-100">
 
             <ul>
+              <li> <a href="#" @click="setContent('purpose')"> Purpose Statement </a> </li>
               <li> <a href="#" @click="setContent('connecting')"> Connecting with Metamask </a> </li>
-            </ul>
+          </ul>
 
           </div>
         </div>
         <div class="w-2/3 bg-gray-300  ">
           <div class="m-6 p-4 bg-gray-100">
 
+
+            <div v-if="activeContent == 'purpose'">
+              <h3 class="text-lg"> 0xBTC Matic Wallet </h3>
+              <br>
+
+              <p> The purpose of this applicaton is to provide a simple front-end user experience that allows users to transfer ERC20 tokens from the Ethereum Mainnet blockchain to the Matic blockchain, a 'Layer 2' network with lower transacting fees and higher bandwidth.  These tokens can also be moved back from the Matic chain back to Ethereum. </p>
+
+            </div>
+
+            <div v-if="activeContent == 'connecting'">
+              <h3 class="text-lg"> Getting Connected </h3>
+              <br>
+
+              <p> To connect to this Dapp, you will need a Web3 compatible browser or extension such as Metamask.    </p>
+
+            </div>
 
 
 
@@ -54,13 +71,14 @@ export default {
   props: [],
   data() {
     return {
-
+      activeContent: 'purpose'
     }
   },
   methods: {
 
     setContent (contentName) {
       console.log('set content', contentName)
+      this.activeContent = contentName;
     }
   }
 }
